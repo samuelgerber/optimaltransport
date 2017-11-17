9ccd82c8b49f04730720aa0b51c1d238c336b844
Samuel Gerber <sgerber@uoregon.edu>
2017-11-16 22:01:55 -0500

    fixed comments in mop package library setup

--- Rpackage/mop/src/mop_config.h
+++ Rpackage/mop/src/mop_config.h
@@ -1,7 +1,7 @@
 //Which linear programming libraries should be made avaiable. Requires for R to
-//have the corresponding libraries on its linker path. Currently all versions
-//are active uncomment if you do not want to suport one of the libraries and
-//adjust the linker arguments in Makevars.
+//have the corresponding libraries on its linker path. Currently lemon is used
+//which is included in the package. If you want to include another library
+//uncomment and adjust the linker arguments in Makevars.
 
 //#define MOP_USE_CPLEX
 //#define MOP_USE_MOSEK
